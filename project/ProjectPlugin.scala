f7f4cb60663724aae345930f276d7ec38d5df38d
Sam Halliday <sam.halliday@gmail.com>
2016-09-12 15:41:44 +0100

    minimise sbt plugins

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -1,5 +1,4 @@
 import com.typesafe.sbt.SbtScalariform.ScalariformKeys
-import de.heikoseeberger.sbtheader.HeaderKey
 import sbt._
 import sbt.Keys._
 import scalariform.formatter.preferences._
@@ -10,7 +9,6 @@ object ProjectPlugin extends SensiblePlugin {
     scalaVersion := "2.11.8",
     organization := "com.fommil",
     version := "1.0.0-SNAPSHOT",
-    HeaderKey.headers := Copyright.ApacheMap,
     ScalariformKeys.preferences := FormattingPreferences().setPreference(AlignSingleLineCaseStatements, true),
     libraryDependencies ++= Sensible.testLibs() ++ Seq(
       "org.scala-lang" % "scala-compiler" % scalaVersion.value,
